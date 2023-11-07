8c2de2416bb2768b0f68a0d290386957b066e43a
Ali <aliahadmd1@gmail.com>
2023-11-07 18:40:41 +0600

    add dto

--- backend-dotnet7/backend-dotnet7/Program.cs
+++ backend-dotnet7/backend-dotnet7/Program.cs
@@ -1,8 +1,27 @@
+using backend_dotnet7.Core.DbContext;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services
+    .AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
+
+// Database connection utilities
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("local");
+    options.UseSqlServer(connectionString);
+});
+
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
